4dcd874e9146d687e1fadcddb78b8a8fc46a53ab
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:24:03 +0400

    task 1.5

--- src/entities/MedicalRecord.java
+++ src/entities/MedicalRecord.java
@@ -12,7 +12,14 @@ public class MedicalRecord implements Displayable {
     private String notes;
     private boolean isConfidential;
 
-
+    private String recordId;
+    private String patientId;
+    private String doctorId;
+    private String visitDate;
+    private String diagnosis;
+    private String prescription;
+    private String notes;
+    private boolean isConfidential;
 
 
 
