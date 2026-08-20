6a493dbf56b186731d170e182f3ecdb13602536a
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 11:40:55 +0400

    task 1.1

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class Person implements Displayable {
 
+
     private String id;
     private String firstName;
     private String lastName;
@@ -17,6 +18,26 @@ public class Person implements Displayable {
     private String nationalId;
     private int age;
     private boolean activeStatus;
+
+
+
+    public Person(String id, String firstName, String lastName,
+                  String dateOfBirth, String gender, String phoneNumber,
+                  String email, String address, String nationalId,
+                  int age, boolean activeStatus) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.nationalId = nationalId;
+        this.age = age;
+        this.activeStatus = activeStatus;
+    }
 
 
 
