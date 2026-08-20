2e3a29a43a998526e180289c6cef53d3b6fef032
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 11:56:12 +0400

    task 1.11

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -1,4 +1,35 @@
 package entities;
 
-public class Patient {
+import java.util.ArrayList;
+
+public class Patient extends Person {
+
+    private String bloodGroup;
+    private String emergencyContact;
+    private String registrationDate;
+
+    private ArrayList allergies;
+    private ArrayList recordIds;
+
+    private double outstandingBalance;
+    private boolean isInsured;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
