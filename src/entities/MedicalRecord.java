d120e1e29cb33bf0d7c7120938c9faabfc449e73
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:23:38 +0400

    task 1.5

--- src/entities/MedicalRecord.java
+++ src/entities/MedicalRecord.java
@@ -1,4 +1,57 @@
 package entities;
 
-public class MedicalRecord {
+import interfaces.Displayable;
+
+public class MedicalRecord implements Displayable {
+    private String recordId;
+    private String patientId;
+    private String doctorId;
+    private String visitDate;
+    private String diagnosis;
+    private String prescription;
+    private String notes;
+    private boolean isConfidential;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
