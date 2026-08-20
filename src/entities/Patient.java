92afcdb0f11ebe11c97873dedd2c8cfae00b5ec8
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:03:00 +0400

    task 1.2

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -1,3 +1,4 @@
+
 package entities;
 
 import java.util.ArrayList;
@@ -14,84 +15,16 @@ public class Patient extends Person {
     private double outstandingBalance;
     private boolean isInsured;
 
-    public Patient(String id, String firstName, String lastName,
-                   String dateOfBirth, String gender, String phoneNumber,
-                   String email, String address, String nationalId,
-                   int age, boolean activeStatus,
-                   String bloodGroup, String emergencyContact,
-                   String registrationDate, double outstandingBalance,
-                   boolean isInsured) {
 
-        super(id, firstName, lastName, dateOfBirth, gender,
-                phoneNumber, email, address, nationalId,
-                age, activeStatus);
 
-        this.bloodGroup = bloodGroup;
-        this.emergencyContact = emergencyContact;
-        this.registrationDate = registrationDate;
-        this.allergies = new ArrayList();
-        this.recordIds = new ArrayList();
-        this.outstandingBalance = outstandingBalance;
-        this.isInsured = isInsured;
-    }
 
-    public Patient(String id, String firstName, String lastName) {
-        super(id, firstName, lastName);
-    }
 
-    public String getBloodGroup() {
-        return bloodGroup;
-    }
 
-    public String getEmergencyContact() {
-        return emergencyContact;
-    }
 
-    public String getRegistrationDate() {
-        return registrationDate;
-    }
 
-    public double getOutstandingBalance() {
-        return outstandingBalance;
-    }
 
-    public boolean isInsured() {
-        return isInsured;
-    }
 
-    public void setBloodGroup(String bloodGroup) {
-        this.bloodGroup = bloodGroup;
-    }
 
-    public void setEmergencyContact(String emergencyContact) {
-        this.emergencyContact = emergencyContact;
-    }
-
-    public void setRegistrationDate(String registrationDate) {
-
-        if (registrationDate == null || registrationDate.trim().isEmpty()) {
-            throw new IllegalArgumentException(
-                    "Registration date cannot be empty."
-            );
-        }
-
-        this.registrationDate = registrationDate;
-    }
-
-    public void setOutstandingBalance(double outstandingBalance) {
-
-        if (outstandingBalance < 0) {
-            throw new IllegalArgumentException(
-                    "Outstanding balance cannot be negative."
-            );
-        }
-
-        this.outstandingBalance = outstandingBalance;
-    }
-
-    public void setInsured(boolean insured) {
-        isInsured = insured;
-    }
 
 
 
@@ -107,3 +40,5 @@ public class Patient extends Person {
 
 
 }
+
+
