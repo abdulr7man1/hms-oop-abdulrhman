5d4735443c03d5f704c436b84cdc526d0ce5bf7e
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:09:32 +0400

    task 1.3

--- src/entities/Doctor.java
+++ src/entities/Doctor.java
@@ -1,4 +1,41 @@
 package entities;
 
-public class Doctor {
+import java.util.ArrayList;
+public class Doctor extends Person {
+
+    // Task 1.3 - Doctor Attributes
+    private String specialization;
+    private int experienceYears;
+    private double consultationFee;
+    private ArrayList availableSlots;
+    private ArrayList assignedPatientIds;
+    private boolean isOnCall;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
