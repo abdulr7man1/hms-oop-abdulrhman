33157d94431d985212be4db1e6fa9e11c3f9e073
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:49:46 +0400

    interface

--- src/interfaces/Displayable.java
+++ src/interfaces/Displayable.java
@@ -1,14 +1,8 @@
 package interfaces;
 
-// ============================================================
-// Task 2.4 - Displayable Interface
-// ============================================================
-
 public interface Displayable {
 
-    // Display all details
     void displayInfo();
 
-    // Display a short summary
     void displaySummary();
 }
--- src/interfaces/Manageable.java
+++ src/interfaces/Manageable.java
@@ -1,4 +1,13 @@
 package interfaces;
 
+
 public interface Manageable {
+
+
+    void add(Object entity);
+
+    boolean removeById(String id);
+
+
+    Object[] getAll();
 }
