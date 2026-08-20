837836f5be94d4ac3202e6629e560cddb6f03008
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:03:18 +0400

    task 1.2

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -14,6 +14,26 @@ public class Patient extends Person {
 
     private double outstandingBalance;
     private boolean isInsured;
+    public Patient(String id, String firstName, String lastName,
+                   String dateOfBirth, String gender, String phoneNumber,
+                   String email, String address, String nationalId,
+                   int age, boolean activeStatus,
+                   String bloodGroup, String emergencyContact,
+                   String registrationDate, double outstandingBalance,
+                   boolean isInsured) {
+
+        super(id, firstName, lastName, dateOfBirth, gender,
+                phoneNumber, email, address, nationalId,
+                age, activeStatus);
+
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.allergies = new ArrayList();
+        this.recordIds = new ArrayList();
+        this.outstandingBalance = outstandingBalance;
+        this.isInsured = isInsured;
+    }
 
 
 
