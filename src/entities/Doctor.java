726bdb10ed49b045c82251befbd6a3842edf004c
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:09:45 +0400

    task 1.9

--- src/entities/Doctor.java
+++ src/entities/Doctor.java
@@ -11,6 +11,24 @@ public class Doctor extends Person {
     private ArrayList assignedPatientIds;
     private boolean isOnCall;
 
+    public Doctor(String id, String firstName, String lastName,
+                  String dateOfBirth, String gender, String phoneNumber,
+                  String email, String address, String nationalId,
+                  int age, boolean activeStatus,
+                  String specialization, int experienceYears,
+                  double consultationFee, boolean isOnCall) {
+
+        super(id, firstName, lastName, dateOfBirth, gender,
+                phoneNumber, email, address, nationalId,
+                age, activeStatus);
+
+        this.specialization = specialization;
+        this.experienceYears = experienceYears;
+        this.consultationFee = consultationFee;
+        this.availableSlots = new ArrayList();
+        this.assignedPatientIds = new ArrayList();
+        this.isOnCall = isOnCall;
+    }
 
 
 
