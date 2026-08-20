b0be6b6b8d8e18a0ccaf38e97f2bfdba60eee4f2
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 11:46:39 +0400

    task 1.2

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -52,6 +52,49 @@ public class Person implements Displayable {
         this.activeStatus = true;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isActiveStatus() {
+        return activeStatus;
+    }
 
 
 
