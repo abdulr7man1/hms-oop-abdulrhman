fda68e840326c9e065362b243e66c49ded7c08fc
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-20 12:31:18 +0400

    task 2.7

--- src/entities/Appointment.java
+++ src/entities/Appointment.java
@@ -1,4 +1,40 @@
 package entities;
+import entities.Appointment;
+import entities.Patient;
+import entities.Doctor;
+import interfaces.Manageable;
+import interfaces.Searchable;
 
-public class Appointment {
+import java.util.ArrayList;
+
+public class AppointmentService
+        implements Manageable, Searchable {
+
+    private ArrayList appointments;
+
+    public AppointmentService() {
+        appointments = new ArrayList();
+    }
+    public Appointment schedule(
+            String appointmentId,
+            String patientId,
+            String doctorId,
+            String appointmentDate) {
+
+        Appointment appointment =
+                new Appointment(
+                        appointmentId,
+                        patientId,
+                        doctorId,
+                        appointmentDate,
+                        "",
+                        "Scheduled",
+                        "",
+                        false
+                );
+
+        add(appointment);
+
+        return appointment;
+    }
 }
